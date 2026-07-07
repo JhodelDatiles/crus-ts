cb7fa3d561e991691397ce80c0f371fd4681a5a6
JhodelDatiles <DatilesJhodel.io@gmail.com>
2026-07-07 11:47:11 +0800

    done setting up rate limiters

---
+++ server/src/config/upstash.ts
@@ -0,0 +1,16 @@
+import { Redis } from '@upstash/redis';
+import { Ratelimit } from '@upstash/ratelimit';
+
+// Initialize the Upstash Redis client
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+})
+
+// Create a new rate limiter that allows 10 requests per 10 seconds
+export const rateLimiter = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(5, "30 s"),
+  analytics: true, // Optional: tracking via Upstash dashboard
+  prefix: "@upstash/ratelimit",
+});
