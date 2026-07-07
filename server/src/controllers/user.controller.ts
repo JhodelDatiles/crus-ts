43cbec6d0976afa53f2d5bf34a22b6bb29886432
JhodelDatiles <DatilesJhodel.io@gmail.com>
2026-07-07 15:17:29 +0800

    fix getAllUser controller

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -26,18 +26,13 @@ export const createUser = async (
 
 // READ (ALL)
 export const getAllUsers = async (
-  req: Request<{ id: string }>,
+  req: Request,
   res: Response,
 ): Promise<void> => {
   try {
-    const user = await User.findById(req.params.id).select("-password");
+    const users = await User.find().select("-password");
 
-    if (!user) {
-      res.status(404).json({ success: false, message: "User not found!" });
-      return;
-    }
-
-    res.status(200).json({ success: true, data: user });
+    res.status(200).json({ success: true, data: users});
   } catch (error: any) {
     res.status(500).json({ success: false, message: error.message });
   }
